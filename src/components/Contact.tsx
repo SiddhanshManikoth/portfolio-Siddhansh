d30826c9de05a73aa6d9788fc56094da935152c4
ShardulVanage <vsharadul@gmail.com>
2025-04-14 12:10:29 +0530

    meatadata change and foem fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,60 +18,32 @@ const defaultFormState = {
 export const Contact = () => {
   const [formData, setFormData] = useState(defaultFormState);
 
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    // Write your submit logic here
-    console.log(formData);
-  };
+ 
   return (
-    <form className="form" onSubmit={handleSubmit}  action="https://getform.io/f/avrwqrya" method="POST">
+    <form className="form"  action="https://getform.io/f/avrwqrya" method="POST">
       <div className="flex flex-col md:flex-row justify-between gap-5">
         <input
           type="text"
+          name="name"
           placeholder="Your Name"
           className="bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 py-2 rounded-md text-sm text-neutral-700 w-full"
-          value={formData.name.value}
-          onChange={(e) => {
-            setFormData({
-              ...formData,
-              name: {
-                value: e.target.value,
-                error: "",
-              },
-            });
-          }}
+        
         />
         <input
           type="email"
+          name="email"
           placeholder="Your email address"
           className="bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 py-2 rounded-md text-sm text-neutral-700 w-full"
-          value={formData.email.value}
-          onChange={(e) => {
-            setFormData({
-              ...formData,
-              email: {
-                value: e.target.value,
-                error: "",
-              },
-            });
-          }}
+    
         />
       </div>
       <div>
         <textarea
           placeholder="Your Message"
+          name="message"
           rows={10}
           className="bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-neutral-200 px-2 mt-4 py-2 rounded-md text-sm text-neutral-700 w-full"
-          value={formData.message.value}
-          onChange={(e) => {
-            setFormData({
-              ...formData,
-              message: {
-                value: e.target.value,
-                error: "",
-              },
-            });
-          }}
+        
         />
       </div>
       <button
